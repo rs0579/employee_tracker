83613cfccccaefa2321f9b24e7c30ed7bb512270
rs0579 <raheemsenegal@outlook.com>
2025-02-01 09:58:02 +0100

    added data to the schema

--- src/db/schema.sql
+++ src/db/schema.sql
@@ -12,7 +12,19 @@ CREATE TABLE department (
 CREATE TABLE role (
     id: SERIAL PRIMARY KEY,
     title: VARCHAR(30),
-    salary: 
+    salary: DECIMAL NOT NULL,
+    department_id: INTEGER,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
 );
 
-CREATE TABLE employee ();
+CREATE TABLE employee (
+    id: SERIAL PRIMARY KEY,
+    first_name: VARCHAR(30),
+    last_name: VARCHAR(30),
+    role_id: INTEGER,
+    manager_id: INTEGER --THIS IS REFERRING TO THE MANAGERS THAT ARE BEING MENTIONED IN THE ROLE TABLE.
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+);
